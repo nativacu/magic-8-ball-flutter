2506ebc4a495caeb805805ceefcc4a1d5654dd9d
nativacu <nativacu@gmail.com>
2022-04-21 23:48:17 -0400

    feat: creating basic scaffold structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask me anything'),
+      ),
+      body: Container(color: Colors.blue),
+    );
+  }
+}
